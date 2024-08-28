a767fc3805c45522c49cfa6a6b15c395215fcf29
xelaoliver <emmet2by4@gmail.com>
2024-08-28 08:03:37 +0100

    Update obj-file-formatter.py
    Works for models with vn& vt.

--- obj-file-formatter.py
+++ obj-file-formatter.py
@@ -1,14 +1,28 @@
-# this only worked with .obj files that their faces were only triangles (idk how other faces would work)
-
-with open('example.obj', 'r') as f:
+with open('3dmodel.obj', 'r') as f:
    obj = []
    for line in f:
       obj.append(line)
 
 output = open("output.txt", "a")
 
+firstFace = True
+
 for i in range(len(obj)):
-   if obj[i][0] == "#":
-      i+=1
-   elif obj[i][0] == "v" and obj[i][1] != "n" and obj[i][1] != "t" or obj[i][0] == "f":
-      output.write(obj[i][1:len(obj[i])].replace(" ",","))
+   if obj[i][0] == "f" or obj[i][0] == "v" and obj[i][1] not in ["n", "t"]:
+      if obj[i][0] == "v":
+         output.write(obj[i][2:].replace(" ", ", ").replace("\n", ", "))
+      else:
+         if firstFace:
+            output.write("\n")
+            firstFace = False
+         f = 0
+         while obj[i][f] != "/":
+            f += 1
+         output.write(obj[i][2:f]+", ")
+         while obj[i][f] != " ":
+            f += 1
+         output.write(obj[i][f+1]+", ")
+         f += 1
+         while obj[i][f] != " ":
+            f += 1
+         output.write(obj[i][f+1]+", ")
