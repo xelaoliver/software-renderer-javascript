c6d4d64e73fcaf1a22cf754eb6451028539644ec
xelaoliver <emmet2by4@gmail.com>
2025-01-17 15:09:02 +0000

    Update monopoly.js
    lol new shtuff here :D :P :00000 looool!

--- monopoly.js
+++ monopoly.js
@@ -10,6 +10,11 @@ var polygons = [
 	[[-25.4, 0, 25.4], [-18.9, 0, 25.4], [-18.9, 0, 18.9], [-25.4, 0, 18.9], [-25.4, 0, 25.4], "#FFFFFF"], // free parking
 	[[-25.4, 0, -25.4], [-18.9, 0, -25.4], [-18.9, 0, -18.9], [-25.4, 0, -18.9], [-25.4, 0, -25.4], "#FFFFFF"], // jail
 	[[25.4, 0, -25.4], [18.9, 0, -25.4], [18.9, 0, -18.9], [25.4, 0, -18.9], [25.4, 0, -25.4], "#FFFFFF"], // go
+	[[22.15, 0, 22.15], ["favicon.png"]], // 2d sprites will be the players
+	[[-22.15, 0, -22.15], ["favicon.png"]],
+	[[-22.15, 0, 22.15], ["favicon.png"]],
+	[[22.15, 0, -22.15], ["favicon.png"]],
+	[[0, -10, 0], ["A video game version of Monopoly but with Capitalism."]] // testing text
 ];
 
 const streets = [
@@ -60,47 +65,75 @@ function calculateVertex(x, y, z) {
 	return [x, y, z];
 }
 
-function calculateAll() {
-    calculatedPolygon = [];
-    calculatedDistance = [];
-    for (let polygonIndex = 0; polygonIndex < polygons.length; polygonIndex++) {
-        let avgDepth = 0;
-        calculatedPolygon.push([]);
-        for (let vertexIndex = 0; vertexIndex < polygons[polygonIndex].length - 1; vertexIndex++) {
-            let item = polygons[polygonIndex][vertexIndex];
-            let vertex = calculateVertex(item[0], item[1], item[2]);
-            avgDepth += vertex[2];
-            calculatedPolygon[polygonIndex].push(vertex);
-        }
-        avgDepth /= polygons[polygonIndex].length-1;
-        calculatedDistance.push({index: polygonIndex, depth: avgDepth});
-        calculatedPolygon[polygonIndex].push(polygons[polygonIndex][polygons[polygonIndex].length-1]);
-    }
-    calculatedDistance.sort((a, b) => b.depth-a.depth);
-    calculatedPolygon = calculatedDistance.map(item => calculatedPolygon[item.index]);
+function calculatePolygons() {
+	calculatedPolygon = [];
+	calculatedDistance = [];
+	for (let polygonIndex = 0; polygonIndex < polygons.length; polygonIndex++) {
+        	let avgDepth = 0;
+        	calculatedPolygon.push([]);
+        	for (let vertexIndex = 0; vertexIndex < polygons[polygonIndex].length - 1; vertexIndex++) {
+            		let item = polygons[polygonIndex][vertexIndex];
+            		let vertex = calculateVertex(item[0], item[1], item[2]);
+            		avgDepth += vertex[2];
+            		calculatedPolygon[polygonIndex].push(vertex);
+        	}
+        	avgDepth /= polygons[polygonIndex].length-1;
+        	
+		calculatedDistance.push({index: polygonIndex, depth: avgDepth});
+        	calculatedPolygon[polygonIndex].push(polygons[polygonIndex][polygons[polygonIndex].length-1]);
+	}
+    	
+	calculatedDistance.sort((a, b) => b.depth-a.depth);
+	
+    	calculatedPolygon = calculatedDistance.map(item => calculatedPolygon[item.index]);
 
-    rotation.x += .02;
-    rotation.y = Math.sin(rotation.x)*0.5;
+    	rotation.x += .02;
+    	rotation.y = Math.sin(rotation.x)*0.5;
 }
 
+/*
+function calculateSprites() {
+	for (let spriteIndex = 0; spriteIndex < sprites.length; spriteIndex ++) {
+		let spriteVertex = calculateVertex(sprites[spriteIndex][0], sprites[spriteIndex][1], sprites[spriteIndex][2]);
+		ctx.drawImage(sprite, spriteVertex[0]*(400/spriteVertex[2])+canvas.width/2-15, spriteVertex[1]*(400/spriteVertex[2])+canvas.height/2-27, 30, 30);
+	}
+}
+*/
 
 function drawAll() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (let polygonIndex = 0; polygonIndex < calculatedPolygon.length; polygonIndex ++) {
-		ctx.beginPath();
 	 	let polygonOfPolygon = calculatedPolygon[polygonIndex];
-		for (let vertexIndex = 0; vertexIndex < polygonOfPolygon.length; vertexIndex ++) {
-			let vertexOfPolygon = polygonOfPolygon[vertexIndex];
-			ctx.lineTo(vertexOfPolygon[0]*(400/vertexOfPolygon[2])+canvas.width/2, vertexOfPolygon[1]*(400/vertexOfPolygon[2])+canvas.height/2);	
+		if (polygonOfPolygon.length == 2) {
+			let vertexOfSprite = polygonOfPolygon[0];
+			if (polygonOfPolygon[1] == "favicon.png") {
+				let sprite = new Image(); sprite.src = polygonOfPolygon[1];
+				ctx.drawImage(sprite, vertexOfSprite[0]*(400/vertexOfSprite[2])+canvas.width/2-15, vertexOfSprite[1]*(400/vertexOfSprite[2])+canvas.height/2-27, 30, 30);
+			} else {
+				ctx.fillStyle = "#000000";
+				ctx.font = "16px serif";
+				ctx.fillText(polygonOfPolygon[1], vertexOfSprite[0]*(400/vertexOfSprite[2])+canvas.width/2-(ctx.measureText(polygonOfPolygon[1]).width/2), vertexOfSprite[1]*(400/vertexOfSprite[2])+canvas.height/2);
+			}
+		} else {
+			ctx.beginPath();
+			for (let vertexIndex = 0; vertexIndex < polygonOfPolygon.length; vertexIndex ++) {
+				let vertexOfPolygon = polygonOfPolygon[vertexIndex];
+				ctx.lineTo(vertexOfPolygon[0]*(400/vertexOfPolygon[2])+canvas.width/2, vertexOfPolygon[1]*(400/vertexOfPolygon[2])+canvas.height/2);	
+			}
+			ctx.fillStyle = polygonOfPolygon[polygonOfPolygon.length-1];
+			ctx.fill();
+			ctx.stroke();
 		}
-		ctx.fillStyle = polygonOfPolygon[polygonOfPolygon.length-1];
-		ctx.fill();
-		ctx.stroke();
 	}
 }
 
+const sprite = new Image();
+sprite.src = "favicon.png";
+
 function all() {
-	calculateAll(); drawAll();
+	calculatePolygons(); drawAll();
 }
 
-setInterval(function() { all() }, 1000/60);
+sprite.addEventListener("load", () => {
+	setInterval(function() { all() }, 1000/60);
+});
